3396e64a52949bc6dd0f8aa2e35850143a9f2189
Tobias Ludwig <ludwig@eit.uni-kl.de>
2019-03-07 10:29:29 +0100

    Removed files

--- example/I2C/ESL/Environment.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by ludwig on 04.01.19.
-//
-
-#ifndef PROJECT_ENVIRONMENT_H
-#define PROJECT_ENVIRONMENT_H
-
-#endif //PROJECT_ENVIRONMENT_H
--- example/I2C/ESL/Types.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by ludwig on 04.01.19.
-//
-
-#ifndef PROJECT_TYPES_H
-#define PROJECT_TYPES_H
-
-#endif //PROJECT_TYPES_H
