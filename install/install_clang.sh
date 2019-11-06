cea32d8e0fd48e1b2a8c86e06ae71a20e48ad9db
Tobias Ludwig <ludwig@eit.uni-kl.de>
2019-11-06 16:38:00 +0100

    Updated workflow

---
+++ install/install_clang.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#BINARY LOCATIONS
+SCAM_HOME="/home/runner/work/DeSCAM/DeSCAM" #NO SLASH AT THE END
+CMAKE_BIN="cmake3" #NO SLASH AT THE END
+PYTHON3="python3" #NO SLASH AT THE END
+
+#INSTALLING LLVM/CLANG
+echo -e "\e[30;48;5;82mInstalling LLVM/CLANG\e[0m";
+(cd $SCAM_HOME/install/tmp/build/ &&
+$CMAKE_BIN -DCMAKE_INSTALL_PREFIX:PATH=$SCAM_HOME/install/tmp/files/llvm ../llvm
+make -w -j4 &&
+make install &&
+cd ../ &&
+rm -r ./build/* &&
+echo -e "\e[30;48;5;82mInstalled LLVM/Clang!\e[0m") || (echo -e "\e[41mError installing LLVM\e[0m" && exit 1) ;
+
+
+
+
