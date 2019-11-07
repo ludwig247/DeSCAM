cdb6d03c46179488f48b7740b7424bffb8346c0b
Tobias Ludwig <ludwig@eit.uni-kl.de>
2019-11-07 13:22:04 +0100

    working on actions

--- example/tests/SlaveDaveBave.h
+++ example/tests/SlaveDaveBave.h
@@ -9,104 +9,37 @@
 #include "systemc.h"
 #include "../Interfaces/Interfaces.h"
 
-//struct TestBasic0 : public sc_module {
-//    //Sections
-//    enum class Phases {
-//        SECTION_A, SECTION_B
-//    };
-//
-//    Phases phase;
-//    Phases nextphase;
-//
-//
-//    //Constructor
-//    SC_HAS_PROCESS(TestBasic0);
-//
-//    TestBasic0(sc_module_name name) :
-//            port_out("b_out"),
-//            port_in("b_out") {
-//        SC_THREAD(fsm);
-//    }
-//
-//    //Out-port
-//    blocking_out<int> port_out;
-//    blocking_in<int> port_in;
-//
-//    //
-//    int var,cnt,result;
-//
-//
-//    void fsm() {
-//        phase = Phases::SECTION_A;
-//        nextphase = Phases::SECTION_A;
-//        while (true) {
-//            phase = nextphase;
-//            if (phase == Phases::SECTION_A) {
-//                port_in->read(var);
-//                ++var;
-//                ++cnt;
-//                result = var+ cnt;
-//                port_out->write(result);
-//                nextphase = Phases::SECTION_B;
-//            }
-//            if (phase == Phases::SECTION_B) {
-//                nextphase = Phases::SECTION_A;
-//            }
-//        }
-//    }
-//};
+SC_MODULE(SlaveTest)
+{
+    slave_in<int> test_in;
+    slave_out<int> test_out;
+    enum Sections{S_A, S_B};
+    Sections section, nextsection;
 
+    int val;
 
-
-
-struct test_compound {
-    int x;
-    unsigned int y;
-//    int y;
-};
-
-struct TestBasic22 : public sc_module {
-    //Sections
-    enum Phases { SECTION_A, SECTION_B };
-    Phases phase, nextphase;
-
-    //Constructor
-    SC_HAS_PROCESS(TestBasic22);
-
-    TestBasic22(sc_module_name name) :
-            m_out("m_out"),
-            test2(30) {
+    SC_CTOR(SlaveTest):
+            test_in("test_in"),
+            test_out("test_out"),
+            nextsection(S_A),
+            val(0)
+    {
         SC_THREAD(fsm);
     }
-
-    //Out-port
-    master_out<test_compound> m_out;
-    //Vars
-    test_compound test;
-    unsigned int test2;
-//    int test2;
-
-    // TODO: an issue with conditions optimization
-    void fsm() {
-        nextphase = SECTION_A;
-        while (true) {
-            phase = nextphase;
-            if (phase == SECTION_A) {
-                //test.set(9);
-                ++test.y;
-                test.y = test2;
-                ++test2;
-                //--test;
-                if (test.y > 10) {
-                    test.y = test2;
-                    m_out->master_write(test);
-                } else
-                    m_out->master_write(test);
-
-                nextphase = SECTION_B;
+    void fsm()
+    {
+        while(true)
+        {
+            section = nextsection;
+            if (section == S_A)
+            {
+                test_in->slave_read(val);
+                test_out->slave_write(val);
+                nextsection = S_B;
             }
-            if (phase == SECTION_B) {
-                nextphase = SECTION_A;
+            if (section == S_B)
+            {
+                nextsection = S_A;
             }
         }
     }
