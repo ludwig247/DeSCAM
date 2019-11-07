9f3e98647dc77f080f6e7b2da509d5917193352e
Tobias Ludwig <ludwig@eit.uni-kl.de>
2019-11-07 08:45:26 +0100

    working on actions

--- install/install_cleanup.sh
+++ install/install_cleanup.sh
@@ -36,19 +36,7 @@ echo "#define SCAM_CONFIG_HOME_H" &&
 echo "#define SCAM_HOME \"$SCAM_HOME\"" &&
 echo "#endif") > $SCAM_HOME/src/global/Config.h;
 
-echo -e "\e[30;48;5;82mInstalling SCAM ...\e[0m";
-(cd $SCAM_HOME/install/tmp/build/ &&
-$CMAKE_BIN ../../../ &&
-$CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target SCAM -- -j 4  &&
-echo -e"\e[30;48;5;82mInstalled SCAM!\e[0m" ) || (echo -e "\e[41mError installing SCAM \e[0m" && exit 1);
 
-echo -e "\e[30;48;5;82mRun Test\e[0m";
-($CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target PrintITL_Test_Run -- -j 4 && $SCAM_HOME/bin/PrintITL_Test_Run)
-
-#DELETE install files
-rm -rf $SCAM_HOME/install/tmp
-
-echo -e "\e[30;48;5;82mDone!\e[0m";
 
 
 
