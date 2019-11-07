a6c4f7bdfbe686292f11fb434de46178a326176d
Tobias Ludwig <ludwig@eit.uni-kl.de>
2019-11-07 10:21:21 +0100

    working on actions

--- install/install_descam.sh
+++ install/install_descam.sh
@@ -9,7 +9,7 @@ PYTHON3="python3" #NO SLASH AT THE END
 echo -e "\e[30;48;5;82mInstalling DESCAM ...\e[0m";
 (cd $SCAM_HOME/install/tmp/build/ &&
 $CMAKE_BIN ../../../ &&
-$CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target SCAM -- -j 4  &&
+$CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target DESCAM -- -j 4  &&
 echo -e"\e[30;48;5;82mInstalled DESCAM!\e[0m" ) || (echo -e "\e[41mError installing DESCAM \e[0m" && exit 1);
 
 
