1aba73c2071a1396b82524cdf4516281609f0c69
Tobias Ludwig <ludwig@eit.uni-kl.de>
2019-11-08 09:43:23 +0100

    more action testing

--- install/install_clang.sh
+++ install/install_clang.sh
@@ -9,6 +9,8 @@ PYTHON3="python3" #NO SLASH AT THE END
 echo -e "\e[30;48;5;82mInstalling LLVM/CLANG\e[0m";
 (cd $SCAM_HOME/install/tmp/build/ &&
 $CMAKE_BIN -DCMAKE_INSTALL_PREFIX:PATH=$SCAM_HOME/install/tmp/files/llvm ../llvm
+gsed -i 's/__msan_unpoison/\/\/__msan_unpoison/' /llvm/lib/Support/Compression.cpp
+gsed -i 's/__msan_allocated_memory/\/\/__msan_allocated_memory/' /llvm/lib/Support/Allocator.cpp
 make -w -j4 &&
 make install &&
 cd ../ &&
