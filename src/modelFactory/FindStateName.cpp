f7968e61244709ac9a1256c01ffd5fd73ba7146e
Tobias Ludwig <ludwig@eit.uni-kl.de>
2019-11-25 17:22:57 +0100

    Weired behavior in debug mode

---
+++ src/modelFactory/FindStateName.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tobias on 25.11.19.
+//
+
+#include "FindStateName.h"
---
+++ src/modelFactory/FindStateName.h
@@ -0,0 +1,14 @@
+//
+// Created by tobias on 25.11.19.
+//
+
+#ifndef DESCAM_FINDSTATENAME_H
+#define DESCAM_FINDSTATENAME_H
+
+
+class FindStateName {
+
+};
+
+
+#endif //DESCAM_FINDSTATENAME_H
