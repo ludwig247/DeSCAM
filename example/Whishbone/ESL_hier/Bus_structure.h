f2223b8b48ebb2d009c9f05367d52885ca74d665
Tobias Ludwig <ludwig@eit.uni-kl.de>
2020-01-30 11:33:38 +0100

    Added hierarchy to Whisbone

--- example/Whishbone/ESL_hier/Bus_structure.h
+++ example/Whishbone/ESL_hier/Bus_structure.h
@@ -17,44 +17,82 @@
 struct Bus_structure : public sc_module {
 
     //Constructor
-    SC_CTOR(Bus_structure):
-            master_in("master_in"),
-            master_out("master_out"),
-            slave_out0("slave_out0"),
-            slave_out1("slave_out1"),
-            slave_out2("slave_out2"),
-            slave_out3("slave_out3"),
-            slave_in0("slave_in0"),
-            slave_in1("slave_in1"),
-            slave_in2("slave_in2"),
-            slave_in3("slave_in3"),
+    SC_CTOR(Bus_structure) :
             masterAgent("masterAgent"),
             slaveAgent0("slaveAgent0"),
             slaveAgent1("slaveAgent1"),
             slaveAgent2("slaveAgent2"),
             slaveAgent3("slaveAgent3"),
             interconnect("interconnect"),
-            masterAgentToBus("masterAgentToBus")
+            masterAgentToBus("masterAgentToBus"),
+            busToMasterAgent("busToMasterAgent"),
+            InterConnToSlaveAgent0("InterConnToSlaveAgent0"),
+            InterConnToSlaveAgent1("InterConnToSlaveAgent1"),
+            InterConnToSlaveAgent2("InterConnToSlaveAgent2"),
+            InterConnToSlaveAgent3("InterConnToSlaveAgent3"),
+            SlaveAgentTOInterconn0("SlaveAgentTOInterconn0"),
+            SlaveAgentTOInterconn1("SlaveAgentTOInterconn1"),
+            SlaveAgentTOInterconn2("SlaveAgentTOInterconn2"),
+            SlaveAgentTOInterconn3("SlaveAgentTOInterconn3"),
+            MasterAgenToOutside("MasterAgenToOutside"),
+            OutsideToMasterAgent("OutsideToMasterAgent"),
+            SlaveToOutside0("SlaveToOutside0"),
+            OutsideToSlave0("OutsideToSlave0"),
+            SlaveToOutside1("SlaveToOutside1"),
+            OutsideToSlave1("OutsideToSlave1"),
+            SlaveToOutside2("SlaveToOutside2"),
+            OutsideToSlave2("OutsideToSlave2"),
+            SlaveToOutside3("SlaveToOutside3"),
+            OutsideToSlave3("OutsideToSlave3")
             {
-                //BINDINGS
-                masterAgent.agent_to_bus(masterAgentToBus);
-                interconnect.master_input(masterAgentToBus);
+
+        //BINDINGS
+        masterAgent.agent_to_bus(masterAgentToBus);
+        interconnect.master_input(masterAgentToBus);
+        masterAgent.bus_to_agent(busToMasterAgent);
+        interconnect.master_output(busToMasterAgent);
+
+        interconnect.slave_out0(InterConnToSlaveAgent0);
+        slaveAgent0.bus_to_agent(InterConnToSlaveAgent0);
+        interconnect.slave_out1(InterConnToSlaveAgent1);
+        slaveAgent1.bus_to_agent(InterConnToSlaveAgent1);
+        interconnect.slave_out2(InterConnToSlaveAgent2);
+        slaveAgent2.bus_to_agent(InterConnToSlaveAgent2);
+        interconnect.slave_out3(InterConnToSlaveAgent3);
+        slaveAgent3.bus_to_agent(InterConnToSlaveAgent3);
+
+
+        interconnect.slave_in0(SlaveAgentTOInterconn0);
+        slaveAgent0.agent_to_bus(SlaveAgentTOInterconn0);
+        interconnect.slave_in1(SlaveAgentTOInterconn1);
+        slaveAgent1.agent_to_bus(SlaveAgentTOInterconn1);
+        interconnect.slave_in2(SlaveAgentTOInterconn2);
+        slaveAgent2.agent_to_bus(SlaveAgentTOInterconn2);
+        interconnect.slave_in3(SlaveAgentTOInterconn3);
+        slaveAgent3.agent_to_bus(SlaveAgentTOInterconn3);
+
+
+        masterAgent.master_to_agent(MasterAgenToOutside);
+        masterAgent.agent_to_master(OutsideToMasterAgent);
+
+        slaveAgent0.agent_to_slave(SlaveToOutside0);
+        slaveAgent0.slave_to_agent(OutsideToSlave0);
+
+        slaveAgent1.agent_to_slave(SlaveToOutside1);
+        slaveAgent1.slave_to_agent(OutsideToSlave1);
+
+        slaveAgent2.agent_to_slave(SlaveToOutside2);
+        slaveAgent2.slave_to_agent(OutsideToSlave2);
+
+        slaveAgent3.agent_to_slave(SlaveToOutside3);
+        slaveAgent3.slave_to_agent(OutsideToSlave3);
+
+
     }
 
-    //In-port
-    blocking_in<bus_req_t> master_in;
-    blocking_in<bus_resp_t> slave_in0;
-    blocking_in<bus_resp_t> slave_in1;
-    blocking_in<bus_resp_t> slave_in2;
-    blocking_in<bus_resp_t> slave_in3;
 
 
-    //Out-por
-    blocking_out<bus_resp_t> master_out;
-    blocking_out<bus_req_t> slave_out0;
-    blocking_out<bus_req_t> slave_out1;
-    blocking_out<bus_req_t> slave_out2;
-    blocking_out<bus_req_t> slave_out3;
+
 
     //sc_module_name name = "test";
     MasterAgent masterAgent;
@@ -63,41 +101,28 @@ struct Bus_structure : public sc_module {
     SlaveAgent slaveAgent2;
     SlaveAgent slaveAgent3;
     Interconnect interconnect;
-//
-//    //Bus
-    Shared<master_signals> masterAgentToBus;
 
-//    interconnect.master_input(masterAgentToBus);
-//    Shared<slave_signals> busToMasterAgent("busToMasterAgent");
-//    masterAgent.bus_to_agent(busToMasterAgent);
-//    interconnect.master_output(busToMasterAgent);
-//
-//    Shared<master_signals> InterConnToSlaveAgent0("InterConnToSlaveAgent0");
-//    interconnect.slave_out0(InterConnToSlaveAgent0);
-//    slaveAgent0.bus_to_agent(InterConnToSlaveAgent0);
-//    Shared<master_signals> InterConnToSlaveAgent1("InterConnToSlaveAgent1");
-//    interconnect.slave_out1(InterConnToSlaveAgent1);
-//    slaveAgent1.bus_to_agent(InterConnToSlaveAgent1);
-//    Shared<master_signals> InterConnToSlaveAgent2("InterConnToSlaveAgent2");
-//    interconnect.slave_out2(InterConnToSlaveAgent2);
-//    slaveAgent2.bus_to_agent(InterConnToSlaveAgent2);
-//    Shared<master_signals> InterConnToSlaveAgent3("InterConnToSlaveAgent3");
-//    interconnect.slave_out3(InterConnToSlaveAgent3);
-//    slaveAgent3.bus_to_agent(InterConnToSlaveAgent3);
-//
-//
-//    Shared<slave_signals> SlaveAgentTOInterconn0("SlaveAgentTOInterconn0");
-//    interconnect.slave_in0(SlaveAgentTOInterconn0);
-//    slaveAgent0.agent_to_bus(SlaveAgentTOInterconn0);
-//    Shared<slave_signals> SlaveAgentTOInterconn1("SlaveAgentTOInterconn1");
-//    interconnect.slave_in1(SlaveAgentTOInterconn1);
-//    slaveAgent1.agent_to_bus(SlaveAgentTOInterconn1);
-//    Shared<slave_signals> SlaveAgentTOInterconn2("SlaveAgentTOInterconn2");
-//    interconnect.slave_in2(SlaveAgentTOInterconn2);
-//    slaveAgent2.agent_to_bus(SlaveAgentTOInterconn2);
-//    Shared<slave_signals> SlaveAgentTOInterconn3("SlaveAgentTOInterconn3");
-//    interconnect.slave_in3(SlaveAgentTOInterconn3);
-//    slaveAgent3.agent_to_bus(SlaveAgentTOInterconn3);
+
+    Shared<master_signals> masterAgentToBus;
+    Shared<slave_signals> busToMasterAgent;
+    Shared<master_signals> InterConnToSlaveAgent0;
+    Shared<master_signals> InterConnToSlaveAgent1;
+    Shared<master_signals> InterConnToSlaveAgent2;
+    Shared<master_signals> InterConnToSlaveAgent3;
+    Shared<slave_signals> SlaveAgentTOInterconn0;
+    Shared<slave_signals> SlaveAgentTOInterconn1;
+    Shared<slave_signals> SlaveAgentTOInterconn2;
+    Shared<slave_signals> SlaveAgentTOInterconn3;
+    Blocking<bus_req_t> MasterAgenToOutside;
+    Blocking<bus_resp_t> OutsideToMasterAgent;
+    Blocking<bus_req_t> SlaveToOutside0;
+    Blocking<bus_resp_t> OutsideToSlave0;
+    Blocking<bus_req_t> SlaveToOutside1;
+    Blocking<bus_resp_t> OutsideToSlave1;
+    Blocking<bus_req_t> SlaveToOutside2;
+    Blocking<bus_resp_t> OutsideToSlave2;
+    Blocking<bus_req_t> SlaveToOutside3;
+    Blocking<bus_resp_t> OutsideToSlave3;
 
 };
 
--- example/Whishbone/ESL_hier/sc_main.cpp
+++ example/Whishbone/ESL_hier/sc_main.cpp
@@ -1,7 +1,4 @@
 #include "systemc.h"
-#include "MasterAgent.h"
-#include "SlaveAgent.h"
-#include "Interconnect.h"
 #include "SlaveDummy.h"
 #include "MasterDummy.h"
 #include "Bus_structure.h"
@@ -9,57 +6,29 @@
 using namespace std;
 
 int sc_main(int, char **) {
+    SlaveDummy slaveDummy0("slaveDummy0");
+    SlaveDummy slaveDummy1("slaveDummy1");
+    SlaveDummy slaveDummy2("slaveDummy2");
+    SlaveDummy slaveDummy3("slaveDummy3");
 
-//
-//    //std::cout << sc_delta_count() << std::endl;
-//    //Connection to Dummies
-//    MasterDummy masterDummy("masterDummy");
-//
-//    SlaveDummy slaveDummy0("slaveDummy0");
-//    SlaveDummy slaveDummy1("slaveDummy1");
-//    SlaveDummy slaveDummy2("slaveDummy2");
-//    SlaveDummy slaveDummy3("slaveDummy3");
-//
-//    Blocking<bus_req_t> MasterDummyToMasterAgent("MasterDummyToMasterAgent");
-//    masterAgent.master_to_agent(MasterDummyToMasterAgent);
-//    masterDummy.bus_req(MasterDummyToMasterAgent);
-//    Blocking<bus_resp_t> MasterAgenToMasterDummy("MasterAgenToMasterDummy");
-//    masterAgent.agent_to_master(MasterAgenToMasterDummy);
-//    masterDummy.bus_resp(MasterAgenToMasterDummy);
-//
-//
-//    Blocking<bus_req_t> SlaveDummyToSlaveAgent0("SlaveDummyToSlaveAgent0");
-//    slaveAgent0.agent_to_slave(SlaveDummyToSlaveAgent0);
-//    slaveDummy0.bus_req(SlaveDummyToSlaveAgent0);
-//    Blocking<bus_resp_t> SlaveAgenToSlaveDummy0("SlaveAgenToSlaveDummy0");
-//    slaveAgent0.slave_to_agent(SlaveAgenToSlaveDummy0);
-//    slaveDummy0.bus_resp(SlaveAgenToSlaveDummy0);
-//
-//
-//
-//    Blocking<bus_req_t> SlaveDummyToSlaveAgent1("SlaveDummyToSlaveAgent1");
-//    slaveAgent1.agent_to_slave(SlaveDummyToSlaveAgent1);
-//    slaveDummy1.bus_req(SlaveDummyToSlaveAgent1);
-//    Blocking<bus_resp_t> SlaveAgenToSlaveDummy1("SlaveAgenToSlaveDummy1");
-//    slaveAgent1.slave_to_agent(SlaveAgenToSlaveDummy1);
-//    slaveDummy1.bus_resp(SlaveAgenToSlaveDummy1);
-//
-//    Blocking<bus_req_t> SlaveDummyToSlaveAgent2("SlaveDummyToSlaveAgent2");
-//    slaveAgent2.agent_to_slave(SlaveDummyToSlaveAgent2);
-//    slaveDummy2.bus_req(SlaveDummyToSlaveAgent2);
-//    Blocking<bus_resp_t> SlaveAgenToSlaveDummy2("SlaveAgenToSlaveDummy2");
-//    slaveAgent2.slave_to_agent(SlaveAgenToSlaveDummy2);
-//    slaveDummy2.bus_resp(SlaveAgenToSlaveDummy2);
-//
-//
-//
-//    Blocking<bus_req_t> SlaveDummyToSlaveAgent3("SlaveDummyToSlaveAgent3");
-//    slaveAgent3.agent_to_slave(SlaveDummyToSlaveAgent3);
-//    slaveDummy3.bus_req(SlaveDummyToSlaveAgent3);
-//    Blocking<bus_resp_t> SlaveAgenToSlaveDummy3("SlaveAgenToSlaveDummy3");
-//    slaveAgent3.slave_to_agent(SlaveAgenToSlaveDummy3);
-//    slaveDummy3.bus_resp(SlaveAgenToSlaveDummy3);
-//
+    Bus_structure bus("bus");
+    MasterDummy masterDummy("masterDummy");
+
+    masterDummy.bus_req(bus.MasterAgenToOutside);
+    masterDummy.bus_resp(bus.OutsideToMasterAgent);
+
+    slaveDummy0.bus_req(bus.SlaveToOutside0);
+    slaveDummy0.bus_resp(bus.OutsideToSlave0);
+
+    slaveDummy1.bus_req(bus.SlaveToOutside1);
+    slaveDummy1.bus_resp(bus.OutsideToSlave1);
+
+    slaveDummy2.bus_req(bus.SlaveToOutside2);
+    slaveDummy2.bus_resp(bus.OutsideToSlave2);
+
+    slaveDummy3.bus_req(bus.SlaveToOutside3);
+    slaveDummy3.bus_resp(bus.OutsideToSlave3);
+
 
 
     sc_start();
