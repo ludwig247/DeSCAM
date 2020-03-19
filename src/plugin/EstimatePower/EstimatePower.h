1e37db344fc837705e8f34c386801d18f73e18c3
Dino Mehmedagić <42766751+nodix95@users.noreply.github.com>
2020-03-19 16:19:29 +0100

    Made PrintSystemC plugin buildable (#56)
    Co-authored-by: nodix95 <dinojezvijer11>

---
+++ src/plugin/EstimatePower/EstimatePower.h
@@ -0,0 +1,48 @@
+//
+// Created by Dino Mehmedagić on 18.03.20.
+//
+
+#include <PluginFactory.h>
+#include <AbstractVisitor.h>
+
+#ifndef DESCAM_ESTIMATEPOWER_H
+#define DESCAM_ESTIMATEPOWER_H
+
+#endif //DESCAM_ESTIMATEPOWER_H
+
+
+class EstimatePower : public PluginFactory, public AbstractVisitor {
+public:
+    EstimatePower() = default;
+
+    ~EstimatePower() = default;
+
+    std::map<std::string, std::string> printModel(Model *node);
+
+private:
+    void visit(Model &node);
+
+    void visit(SCAM::Module &node);
+
+    void visit(ModuleInstance &node);
+
+    void visit(Port &node);
+
+    void visit(DataSignal &node); //not used
+    void visit(Channel &node);
+
+    void visit(Interface &node); //not used
+    void visit(Variable &node);
+
+    void visit(FSM &node);
+
+    void visit(DataType &node);
+
+    void visit(Function &node);
+
+    void visit(Parameter &node);
+
+    void visit(Timepoint &node);
+
+
+};
