92a46de6e31c281501b553d8e1b30f22205636c8
fplasencia <frenkx1996@gmail.com>
2020-03-26 09:27:32 +0100

    Edited Workflow and Edited/Added scripts for github

--- install/github/install_run_tests.sh
+++ install/github/install_run_tests.sh
@@ -6,15 +6,15 @@ CMAKE_BIN="cmake" #NO SLASH AT THE END
 PYTHON3="python3" #NO SLASH AT THE END
 
 echo -e "\e[30;48;5;82mRun PrintITL_Test_Run\e[0m";
-($CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target PrintITL_Test_Run -- -j 4 && $SCAM_HOME/bin/PrintITL_Test_Run)
-
+#($CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target PrintITL_Test_Run -- -j 4 && $SCAM_HOME/bin/PrintITL_Test_Run)
+$SCAM_HOME/bin/PrintITL_Test_Run
 echo -e "\e[30;48;5;82mRun PrintSkeleton_Test_Run\e[0m";
-($CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target PrintSkeleton_Test_Run -- -j 4 && $SCAM_HOME/bin/PrintSkeleton_Test_Run)
-
+#($CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target PrintSkeleton_Test_Run -- -j 4 && $SCAM_HOME/bin/PrintSkeleton_Test_Run)
+$SCAM_HOME/bin/PrintSkeleton_Test_Run
 echo -e "\e[30;48;5;82mRun Optimizer_Test_Run\e[0m";
-($CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target Optimizer_Test_Run -- -j 4 && $SCAM_HOME/bin/PrintSkeleton_Test_Run)
-
-rm -rf $SCAM_HOME/install/tmp
+#($CMAKE_BIN --build $SCAM_HOME/install/tmp/build/ --target Optimizer_Test_Run -- -j 4 && $SCAM_HOME/bin/PrintSkeleton_Test_Run)
+$SCAM_HOME/bin/PrintSkeleton_Test_Run
+#rm -rf $SCAM_HOME/install/tmp
 
 echo -e "\e[30;48;5;82mDone!\e[0m";
 
