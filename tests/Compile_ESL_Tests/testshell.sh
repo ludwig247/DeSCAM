0234dbe702d3d2bb7f2875654998d7a8b4fb55cf
Simon Loevald <simonlovald@gmail.com>
2020-04-14 16:28:53 +0200

    added output-file with error report

--- tests/Compile_ESL_Tests/testshell.sh
+++ tests/Compile_ESL_Tests/testshell.sh
@@ -1,22 +1,21 @@
-echo "This is a shell"
-value=$(<tests.txt)
-echo "$value"
+exec 2> error_file
+#exec 2>&1
 
 file=./tests.txt
-#grep -v '//' < $file|
-#while IFS=" ;" read -r a b c; do
-#    echo a: $a b: $b c: $c
-#done
-
-#mkdir build
-#cd ../../cmake-build-debug
-
 
 egrep -v '^//|^[[:space:]]*$' < $file|
 while IFS= read -r test; do
   cmake --build ../../cmake-build-debug --target $test
 
-    echo ===="$test SUCC"=====
+if [ $? -eq 0 ]; then
+     echo "==== build $test SUCCESS ===="
+else
+     echo "==== build $test FAILED ===="
+fi
 done
-cd ../../cmake-build-debug
-make
+
+#cd ../../cmake-build-debug
+#egrep -v '^//|^[[:space:]]*$' < $file|
+#while IFS= read -r test; do
+#make $test
+#done
