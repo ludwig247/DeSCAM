b1de7aa6853e151ad5e61bf89eb91e16a441731f
Simon Loevald <simonlovald@gmail.com>
2020-04-19 22:04:05 +0200

    Generating wanted JSON-output-file

--- tests/Compile_ESL_Tests/testshell.sh
+++ tests/Compile_ESL_Tests/testshell.sh
@@ -1,21 +1,19 @@
-exec 2> error_file
-#exec 2>&1
-
-file=./tests.txt
-
-egrep -v '^//|^[[:space:]]*$' < $file|
-while IFS= read -r test; do
-  cmake --build ../../cmake-build-debug --target $test
-
-if [ $? -eq 0 ]; then
-     echo "==== build $test SUCCESS ===="
-else
-     echo "==== build $test FAILED ===="
-fi
-done
-
-#cd ../../cmake-build-debug
+#file=./tests.txt
+#
 #egrep -v '^//|^[[:space:]]*$' < $file|
 #while IFS= read -r test; do
-#make $test
+#  exec 2> ./error_files/error_file_$test
+#  cmake --build ../../cmake-build-debug --target $test
+#
+#if [ $? -eq 0 ]; then
+#     echo "==== build $test SUCCESS ===="
+#     export MY_OPTION="TRUE"
+#else
+#     echo "==== build $test FAILED ===="
+#     export THING="FALSE"
+#fi
 #done
+
+cmake --build ../../cmake-build-debug --target Compile_ESL_Test_Run
+
+
