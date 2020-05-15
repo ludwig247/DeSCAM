10b64c265ed3320e49d02d95995695350f4fe041
Simonlovald <simonlovald@gmail.com>
2020-05-15 14:16:28 +0200

    Fixes to install_new.zip

--- tests/Print_ITL_Tests/PrintITL_Test.h
+++ tests/Print_ITL_Tests/PrintITL_Test.h
@@ -12,13 +12,39 @@
 #include <fstream>
 
 #include <PrintITL/PrintITL.h>
-//
-//
-//static std::vector<SCAM::Module *> parameter() {
-//
-//
-//    return result;
-//}
+
+
+static std::vector<SCAM::Module *> parameter() {
+
+    std::vector<const char *> commandLineArugmentsVector;
+
+    //Binary
+    std::string bin = std::string(SCAM_HOME"/bin/SCAM ");
+    commandLineArugmentsVector.push_back(bin.c_str());
+
+    //SRC-File to be analyzed
+    std::string file_path = std::string(SCAM_HOME"/tests/Print_ITL_Tests/TestCases/tests.h");
+    commandLineArugmentsVector.push_back(file_path.c_str());
+
+    //Creates an instance of ModelFactory and calls ModelFactory::HandleTranslationUnit
+    const char *commandLineArgumentsArray[commandLineArugmentsVector.size()];
+    for (int i = 0; i < commandLineArugmentsVector.size(); i++) {
+        commandLineArgumentsArray[i] = commandLineArugmentsVector.at(i);
+    }
+
+//    add optimizations
+//    std::set<std::string> optimizeOptions = {"all"};
+//    CommandLineParameter::setOptimizeOptionsSet(optimizeOptions);
+    SCAM::ModelGlobal::createModel(commandLineArugmentsVector.size(), commandLineArgumentsArray[0],commandLineArgumentsArray[1]);
+
+    std::vector<SCAM::Module *> result;
+    for (auto module: SCAM::ModelGlobal::getModel()->getModules()) {
+        SCAM::DataTypes::reset();
+        result.push_back(module.second);
+    }
+
+    return result;
+}
 
 class ITL_Test : public ::testing::TestWithParam<SCAM::Module *> {
 public:
--- tests/Print_ITL_Tests/tests.cpp
+++ tests/Print_ITL_Tests/tests.cpp
@@ -1,24 +1,44 @@
 //
 // Created by tobias on 11.04.17.
 //
-#include <gtest/gtest.h>
-#include <PrintITL/PrintITL.h>
+//#include <gtest/gtest.h>
+//
 //#include "PrintITL_Test.h"
 #include <ModelGlobal.h>
 #include <fstream>
+
+#include <PrintITL/PrintITL.h>
+
+
 //TODO: no need for main
 
-std::vector main(int argc, char **argv) {
-//    testing::InitGoogleTest(&argc, argv);
-//    return RUN_ALL_TESTS();
-
+int main(int argc, char **argv) {
+//   testing::InitGoogleTest(&argc, argv);
+//  return RUN_ALL_TESTS();
     std::vector<const char *> commandLineArugmentsVector;
 
     //Binary
-    std::string bin = std::string(SCAM_HOME"/bin/SCAM ");
+    std::string bin = std::string(SCAM_HOME"/bin/SCAM");
     commandLineArugmentsVector.push_back(bin.c_str());
 
     //SRC-File to be analyzed
+//    std::ifstream ifs(SCAM_HOME"/tests/Print_ITL_Tests/TestCases/tests.h");
+//
+//    std::string line;
+//    std::vector<std::string> header_includes;
+//
+//    while(std::getline(ifs, line))
+//    {
+//
+//        if(line.size() > 0 && !(line.find("//") == 0))
+//            header_includes.push_back(line);
+//    }
+//    for(int i = 0; i < header_includes.size(); i++) {
+//        commandLineArugmentsVector.push_back(header_includes[i].c_str());
+//    }
+
+
+
     std::string file_path = std::string(SCAM_HOME"/tests/Print_ITL_Tests/TestCases/tests.h");
     commandLineArugmentsVector.push_back(file_path.c_str());
 
@@ -26,17 +46,22 @@ std::vector main(int argc, char **argv) {
     const char *commandLineArgumentsArray[commandLineArugmentsVector.size()];
     for (int i = 0; i < commandLineArugmentsVector.size(); i++) {
         commandLineArgumentsArray[i] = commandLineArugmentsVector.at(i);
+        std::cout<<commandLineArugmentsVector[i]<<std::endl;
     }
 
 //    add optimizations
-//    std::set<std::string> optimizeOptions = {"all"};
-//    CommandLineParameter::setOptimizeOptionsSet(optimizeOptions);
-    SCAM::ModelGlobal::createModel(commandLineArugmentsVector.size(), commandLineArgumentsArray[0],commandLineArgumentsArray[1]);
+    std::set<std::string> optimizeOptions = {"all"};
+    CommandLineParameter::setOptimizeOptionsSet(optimizeOptions);
+
+    for (int i = 1; i < commandLineArugmentsVector.size(); i++) {
+        SCAM::ModelGlobal::createModel(commandLineArugmentsVector.size(), commandLineArgumentsArray[0],
+                                       commandLineArgumentsArray[i]);
+    }
 
     std::vector<SCAM::Module *> result;
     for (auto module: SCAM::ModelGlobal::getModel()->getModules()) {
         SCAM::DataTypes::reset();
         result.push_back(module.second);
     }
-    return 0;
+
 }
