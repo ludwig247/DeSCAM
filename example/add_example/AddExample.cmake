18b29baac837d6b607cfdfef6271a62116e91a5d
Simonlovald <simonlovald@gmail.com>
2020-08-06 13:46:28 +0200

    Fixes to cache problem

--- example/add_example/AddExample.cmake
+++ example/add_example/AddExample.cmake
@@ -6,8 +6,8 @@ macro (add_example)
     elseif(${NUM_MACRO_ARG} LESS_EQUAL 2)
         list(GET MACRO_ARG 0 FIRST_ARG)
 #        list(GET MACRO_ARG 1 SECOND_ARG)
-        if(IS_DIRECTORY "${CMAKE_SOURCE_DIR}/example/${FIRST_ARG}")
-            if(EXISTS "${CMAKE_SOURCE_DIR}/example/${FIRST_ARG}/ESL")
+        if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${FIRST_ARG}")
+            if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${FIRST_ARG}/ESL")
                 file(GLOB EXAMPLE_H "${FIRST_ARG}/ESL/*.h")
 #                    message(STATUS "${first_arg}")
             else()
@@ -22,8 +22,8 @@ macro (add_example)
         list(GET MACRO_ARG 0 FIRST_ARG)
 #        list(GET MACRO_ARG 1 SECOND_ARG)
         list(GET MACRO_ARG 2 THIRD_ARG)
-        if(IS_DIRECTORY "${CMAKE_SOURCE_DIR}/example/${THIRD_ARG}")
-            if(EXISTS "${CMAKE_SOURCE_DIR}/example/${THIRD_ARG}/ESL")
+        if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${THIRD_ARG}")
+            if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${THIRD_ARG}/ESL")
                 file(GLOB EXAMPLE_H "${THIRD_ARG}/ESL/*.h")
                 #        message(STATUS "${EXAMPLE_H}")
             else()
@@ -43,40 +43,68 @@ function(create_directory)
     list(GET FUNCT_ARG 0 FIRST_ARG)
     message(WARNING "Given example directory ${FIRST_ARG} does not exist, add_example() will create a new example directory tree")
     set(EXAMPLE_NAME "${FIRST_ARG}_Simulation")
-    file(MAKE_DIRECTORY ${CMAKE_SOURCE_DIR}/example/${FIRST_ARG}/ESL/env ${CMAKE_SOURCE_DIR}/example/${FIRST_ARG}/RTL/properties)
-    file(WRITE ${CMAKE_SOURCE_DIR}/example/${FIRST_ARG}/ESL/env/CMakeLists.txt "file(GLOB ENV_SRC CONFIGURE_DEPENDS *.cpp *.hpp *.h)")
-    file(WRITE ${CMAKE_SOURCE_DIR}/example/${FIRST_ARG}/CMakeLists.txt "add_subdirectory(ESL)")
-#    file(APPEND ./CMakeLists.txt "#add_subdirectory(${FIRST_ARG})\n")
+    file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${FIRST_ARG}/ESL/env ${CMAKE_CURRENT_SOURCE_DIR}/${FIRST_ARG}/RTL/properties)
+    file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/${FIRST_ARG}/ESL/env/CMakeLists.txt "file(GLOB ENV_SRC CONFIGURE_DEPENDS *.cpp *.hpp *.h)")
+    file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/${FIRST_ARG}/CMakeLists.txt "add_subdirectory(ESL)")
+    file(APPEND ./add_subdirectory.cmake "add_subdirectory(${FIRST_ARG})\n")
     configure_file ( #Creates new example header-file
-            "templates/template_h.h.in"
-            "${CMAKE_SOURCE_DIR}/example/${FIRST_ARG}/ESL/${FIRST_ARG}.h" @ONLY)
+            "./add_example/templates/template_h.h.in"
+            "${CMAKE_CURRENT_SOURCE_DIR}/${FIRST_ARG}/ESL/${FIRST_ARG}.h" @ONLY)
     configure_file ( #Creates CMakeLists for the ESL-folder
-            "templates/New_Example_CMakeLists.txt.in"
-            "${CMAKE_SOURCE_DIR}/example/${FIRST_ARG}/ESL/CMakeLists.txt" @ONLY)
+            "./add_example/templates/CMakeLists.txt.in"
+            "${CMAKE_CURRENT_SOURCE_DIR}/${FIRST_ARG}/ESL/CMakeLists.txt" @ONLY)
     configure_file ( #Creates a sc_main.cpp for example
-            "templates/template_sc_main.cpp.in"
-            "${CMAKE_SOURCE_DIR}/example/${FIRST_ARG}/ESL/env/sc_main.cpp" @ONLY)
+            "./add_example/templates/template_sc_main.cpp.in"
+            "${CMAKE_CURRENT_SOURCE_DIR}/${FIRST_ARG}/ESL/env/sc_main.cpp" @ONLY)
 endfunction(create_directory)
 
 macro(update_list)
 #    message(STATUS "LIST: ${EXAMPLE_NAMES_LIST}")
-    set(CMAKELISTS "${CMAKE_SOURCE_DIR}/example/add_example/CMakeLists.txt")
+    set(CMAKELISTS "${CMAKE_SOURCE_DIR}/example/add_subdirectory.cmake")
     file(STRINGS "${CMAKELISTS}" ADDED_EXAMPLES)
+
 #   Wraps each Name in the list of names passed by add_example
 #   to match with contents of the add_subdirectory-list in CMakeLists.txt:
     foreach(EXAMPLE_NAME_SUB ${EXAMPLE_NAMES_LIST})
-        if(EXISTS "${CMAKE_SOURCE_DIR}/example/${EXAMPLE_NAME_SUB}/CMakeLists.txt")
-            set(EXAMPLE_SUB_LIST ${EXAMPLE_SUB_LIST} "add_subdirectory(${EXAMPLE_NAME_SUB})\n")
+        set(EXAMPLE_SUB_LIST ${EXAMPLE_SUB_LIST} "add_subdirectory(${EXAMPLE_NAME_SUB})")
+    endforeach()
+    foreach(EXAMPLE_NAME_SUB ${EXAMPLE_NAMES_LIST})
+        set(EXAMPLE_DISABLED_SUB_LIST ${EXAMPLE_DISABLED_SUB_LIST} "#add_subdirectory(${EXAMPLE_NAME_SUB})")
+    endforeach()
+#    message(STATUS "${ADDED_EXAMPLES}")
+    #    message(STATUS "${EXAMPLE_SUB_LIST}")
+    foreach(LINE IN LISTS ADDED_EXAMPLES)
+        #    message(STATUS "not modified: ${LINE}")
+        if(LINE MATCHES "^add_subdirectory.")
+            list(FIND EXAMPLE_SUB_LIST "${LINE}" EXAMPLE_ADDED)
+            #        message(STATUS ${EXAMPLE_ADDED})
+            if (EXAMPLE_ADDED EQUAL -1)
+                string(REGEX REPLACE "^[a]" "#a" LINE ${LINE})
+                #            message(STATUS "modified: ${LINE}")
+            endif ()
+            list(APPEND MODIFIED "${LINE}\n")
+        elseif(LINE MATCHES "^#add_subdirectory.")
+            list(FIND EXAMPLE_DISABLED_SUB_LIST "${LINE}" EXAMPLE_DISABLED)
+            #        message(STATUS ${EXAMPLE_DISABLED})
+            if (EXAMPLE_DISABLED GREATER -1)
+                string(REGEX REPLACE "^#" "" LINE ${LINE})
+                #            message(STATUS "modified: ${LINE}")
+            endif ()
+            list(APPEND MODIFIED "${LINE}\n")
+            #        message(STATUS "${MODIFIED}")
+        else()
+#            string (REGEX REPLACE ";" "\\\\;" LINE "${LINE}")
+#            list(APPEND MODIFIED "${LINE}\n")
+            #        message(STATUS "modified: " ${MODIFIED})
         endif()
     endforeach()
-string (REGEX REPLACE ";" "" EXAMPLE_SUB_LIST "${EXAMPLE_SUB_LIST}")
 
-#    message(STATUS ${EXAMPLE_SUB_LIST})
+string(REGEX REPLACE "^;" "" MODIFIED ${MODIFIED})
+#message(STATUS "This is: \n" "${MODIFIED}")
 
 #   Remakes the CMakeLists.txt, updating the add_subdirectory-list
-configure_file (
-#        "./add_example/templates/CMakeLists.txt.in"
-        "./templates/CMakeLists.txt.in"
-        "${CMAKE_SOURCE_DIR}/example/CMakeLists.txt" @ONLY)
+configure_file(
+        ${CMAKE_SOURCE_DIR}/example/add_example/templates/add_subdirectory.txt.in
+        ${CMAKE_SOURCE_DIR}/example/add_subdirectory.cmake)
 
 endmacro(update_list)
--- example/add_example/Readme.md
+++ example/add_example/Readme.md
@@ -1,6 +1,8 @@
 # AddExample-macro
 
 The AddExample-macro provides the user with features that eases the adding of a new example to the DeSCAM-Project.
+In order to add a new example include it in the [Examples.cmake](../Examples.cmake) and the AddExample-macro will do the rest of the work.
+Comment/uncomment the add_example() in order to deactivate/activate the macro for one particular example.
 
 **These features include:**
 * Providing a list of the header-files required to run the "Print-ITL-Tests" on the example.
@@ -8,8 +10,7 @@ The AddExample-macro provides the user with features that eases the adding of a 
 which builds to test if all examples builds successfully and produces a report in the format of a json-script.
 * Creating a new template directory tree including required CMakeLists.txt-files 
 when adding new example.
-* Automatic comment out or uncomment add_subdirectory in [CMakeLists.txt](../CMakeLists.txt) according to if the respective add_example is activated or not.
-* ***Reminder:*** Always reload CMake twice after uncommenting an add_example-macro. This is for CMake to notice the change in the add_subdirectory-list.
+* Automatic comment out or uncomment add_subdirectory in [add_subdirectory.cmake](../add_subdirectory.cmake) according to if the respective add_example is activated or not.
 
 ***Important remarks:***
 The directory tree of the example should look like this:
@@ -32,7 +33,7 @@ in the new example named "env", the rest in parent-folder "ESL"
 it is important that the target specified in the "add_executable()" ends with  "_Simulation", e.g. "myexample_simulation",
 otherwise the test-build will fail.
 
-To include a new example, add the macro "add_example()" to the [CMakeLists.txt](../CMakeLists.txt) in the parent "example"-directory, 
+To include a new example, add the macro "add_example()" to the [Example.cmake](../Example.cmake) in the parent "example"-directory, 
 specifying name, description and example directory.
 
 *example: add_example(PrintMYStyle; PrintITL; MYStyleDirectory)*
@@ -40,6 +41,6 @@ specifying name, description and example directory.
 Note that if the name of the example directory is not specified, 
 the macro will assume that the name of the directory is the same as the name of the example.
 
-Add_example automatically adds "add_subdirectory()" for the new example to the [CMakeLists.txt](../CMakeLists.txt) in the parent "example"-directory
+Add_example automatically adds "add_subdirectory()" for the new example to the [add_subdirectory.cmake](../add_subdirectory.cmake) in the parent "example"-directory
 for Cmake to recognize the build-target.
 CMake will comment out/uncomment the add_subdirectory() according to if the add_example for the example is commented out or not.
