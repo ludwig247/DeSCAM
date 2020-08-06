2e55e6046d62b8089fba75c03f51fd9f9e6333c8
Tobias Ludwig <ludwig@eit.uni-kl.de>
2020-08-06 16:11:17 +0200

    Moving some files around

--- example/Example.cmake
+++ example/Example.cmake
@@ -7,15 +7,15 @@ include(add_example/AddExample.cmake)
 #
 add_example(Bus; Print_ITL_Tests )
 #add_example(FIFO; Print_ITL_Tests)
-#add_example(FPI; Print_ITL_Tests)
-#add_example(I2C; Print_ITL_Tests)
-#add_example(Framer; Print_ITL_Tests)
-#add_example(Paper; Print_ITL_Tests)
+add_example(FPI; Print_ITL_Tests)
+add_example(I2C; Print_ITL_Tests)
+add_example(Framer; Print_ITL_Tests)
+add_example(Paper; Print_ITL_Tests)
 #add_example(operation-pipeline; Print_ITL_Tests)
-#add_example(RISCV_ISA)
-#add_example(Whishbone)
+add_example(RISCV_ISA)
+add_example(Whishbone)
 #add_example(example_test)
-#add_example(does_not_make_sense; A_Test; nonsense)
+
 #
 # Updates the list of add_subdirectory according to the add_example macros activated
 update_list()
