d592a217cb034d40834f4ddf73bc8082d3a3fb77
J3s7r <72141858+J3s7r@users.noreply.github.com>
2020-10-07 17:06:38 +0200

    Soft 24 (#121)
    * Added test cases to test GMOCK
    
    * Added missing files for TestCase
    
    * Soft sprint 2 (#120)
    
    * Deleted FindCall, because it's never used
    
    * FindDataFlow now dependent on IFindDataFlow for DIP
    
    * Renamed IFindDataFLow.h to IFindDataFlow.h
    
    * Implemented IFindFunctions for implementing DIP into FindFunctions
    
    * SOFT-20: Implemented Factory
    
    * SOFT-22: Factory Implemented
    
    * Soft-20 & Soft-22: Documentation and style
    
    * Soft-26
    
    * Deleted FindInterface because it's not used
    
    * SOFT22 + SOFT26
    
    * Merged Master
    
    Co-authored-by: Tim <tim.burr@lubis-eda.com>
    
    * SOFT-24:
    Interface for FindGlobal.h
    Switch in ModelFactory.cpp
    
    Co-authored-by: Tobias Ludwig <ludwig@eit.uni-kl.de>
    Co-authored-by: potatojuicemachine <49519532+potatojuicemachine@users.noreply.github.com>
    Co-authored-by: Tim <tim.burr@lubis-eda.com>

---
+++ tests/GMock_Test/tests/TestCase3.h
@@ -0,0 +1,37 @@
+//
+// Created by tobias on 07.10.20.
+//
+
+#ifndef SCAM_TESTS_GMOCK_TEST_TESTS_TESTCASE3_H_
+#define SCAM_TESTS_GMOCK_TEST_TESTS_TESTCASE3_H_
+
+#include "defines.h"
+
+SC_MODULE(TestCase3) {
+    blocking_in<int> test_in;
+    blocking_out<unsigned int> test_out;
+
+    int foo;
+    unsigned int bar;
+    unsigned int list[5];
+
+    SC_CTOR(TestCase3) :
+    test_in("test_in"),
+    test_out("test_out") {
+      SC_THREAD(fsm);
+    }
+
+    void fsm() {
+      while (true) {
+        test_in->read(foo);
+        bar = bar + static_cast<unsigned int>(compute2(foo, bar));
+        bar = bar + compute(3, compute2(foo, bar));
+        bar = bar + compute(list[0], 5);
+        if (compute3(foo > 5, bar)) {
+          test_out->write(bar);
+        } else test_out->write(bar + 1);
+      }
+    }
+};
+
+#endif //SCAM_TESTS_GMOCK_TEST_TESTS_TESTCASE3_H_
