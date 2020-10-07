74f11ae973523f43dfdf0f5a64a19c16149c750b
J3s7r <72141858+J3s7r@users.noreply.github.com>
2020-10-07 17:06:38 +0200

    Soft 24 (#121)
    * Added test cases to test GMOCK
    
    * Added missing files for TestCase
    
    * Soft sprint 2 (#120)
    
    * Deleted FindCall, because it's never used
    
    * FindDataFlow now dependent on IFindDataFlow for DIP
    
    * Renamed IFindDataFLow.h to IFindDataFlow.h
    
    * Implemented IFindFunctions for implementing DIP into FindFunctions
    
    * SOFT-20: Implemented Factory
    
    * SOFT-22: Factory Implemented
    
    * Soft-20 & Soft-22: Documentation and style
    
    * Soft-26
    
    * Deleted FindInterface because it's not used
    
    * SOFT22 + SOFT26
    
    * Merged Master
    
    Co-authored-by: Tim <tim.burr@lubis-eda.com>
    
    * SOFT-24:
    Interface for FindGlobal.h
    Switch in ModelFactory.cpp
    
    Co-authored-by: Tobias Ludwig <ludwig@eit.uni-kl.de>
    Co-authored-by: potatojuicemachine <49519532+potatojuicemachine@users.noreply.github.com>
    Co-authored-by: Tim <tim.burr@lubis-eda.com>

--- src/modelFactory/FindGlobal.h
+++ src/modelFactory/FindGlobal.h
@@ -9,33 +9,42 @@
 #include <string>
 #include <Module.h>
 #include "clang/AST/RecursiveASTVisitor.h"
+#include <IFindGlobal.h>
 
-namespace DESCAM{
-    class FindGlobal: public clang::RecursiveASTVisitor<FindGlobal> {
+namespace DESCAM {
+    class FindGlobal : public IFindGlobal, public clang::RecursiveASTVisitor<FindGlobal> {
     public:
-        explicit FindGlobal(clang::TranslationUnitDecl* decl, clang::CompilerInstance &ci);
+        explicit FindGlobal(clang::TranslationUnitDecl *decl, clang::CompilerInstance &ci);
+
         FindGlobal() = delete;
-        ~FindGlobal() = default;
-        virtual bool VisitVarDecl(const clang::VarDecl * varDecl);
-        virtual bool VisitFunctionDecl(const clang::FunctionDecl * funDecl);
 
-        const std::map<std::string, Variable *> &getVariableMap() const;
-        const std::map<std::string, Function *> &getFunctionMap() const;
+        ~FindGlobal() override = default;
 
-        const std::map<std::string, const clang::FunctionDecl *> &getFunctionDeclMap() const;
+        bool setup(clang::TranslationUnitDecl *decl, clang::CompilerInstance &ci) override;
+
+        const std::map<std::string, Variable *> &getVariableMap() const override;
+
+        const std::map<std::string, Function *> &getFunctionMap() const override;
+
+        const std::map<std::string, const clang::FunctionDecl *> &getFunctionDeclMap() const override;
+
+        bool VisitVarDecl(const clang::VarDecl *varDecl);
+
+        bool VisitFunctionDecl(const clang::FunctionDecl *funDecl);
 
     private:
-        clang::CompilerInstance & ci;
-        DESCAM::DataType * getDataType(const clang::QualType& type) const;
-        clang::TranslationUnitDecl* decl;
+        clang::CompilerInstance &ci;
+
+        DESCAM::DataType *getDataType(const clang::QualType &type) const;
+
+        clang::TranslationUnitDecl *decl;
         DESCAM::Module module = Module("placeholder");
-        std::map<std::string,Variable*> variableMap;
-        std::map<std::string,Function*> functionMap;
-        std::map<std::string,const clang::FunctionDecl*> functionDeclMap;
+
+        std::map<std::string, Variable *> variableMap;
+        std::map<std::string, Function *> functionMap;
+        std::map<std::string, const clang::FunctionDecl *> functionDeclMap;
     };
 }
-
-
 
 
 #endif //DESCAM_FINDGLOBAL_H
