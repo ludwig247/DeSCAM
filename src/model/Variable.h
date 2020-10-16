4ce583ae856d006014cb762d0e8123a27bb82851
J3s7r <72141858+J3s7r@users.noreply.github.com>
2020-10-16 09:18:45 +0200

    Soft sprint 2 (#133)
    * Updated googletest version 1.8 -> 1.10
    
    * "setup" function for test -> doesn't work for mock!
    Style Guide and some Clang-Tidy recommendations.

--- src/model/Variable.h
+++ src/model/Variable.h
@@ -13,28 +13,31 @@
 
 namespace DESCAM {
 
-    /** \brief Represnts a variable within the module structure
-     *
-     *  - Variable(std::string name,DataType * type)
-     *  Every variable needs to have an initialValue, if not passed the following initialvalues are assuemd
-     *  bool: false
-     *  int: 0
-     *  enum: the first symbolic value in the enum value list
-     *
-     *  - Variable with built-in type: Variable(std::string name,DataType * type, ConstValue * initialValue);
-     *  - Compound Variable: Create all sub-variables with their initalValue, create a new variable and pass them as a list
-     *
-     */
-    class Variable : public VariableTemplate<Variable> {
-    public:
-        Variable(std::string name, const DataType *type, ConstValue *initialValue = nullptr, Variable *parent = nullptr,LocationInfo locationInfo = LocationInfo());
+/** \brief Represnts a variable within the module structure
+ *
+ *  - Variable(std::string name,DataType * type)
+ *  Every variable needs to have an initialValue, if not passed the following initialvalues are assuemd
+ *  bool: false
+ *  int: 0
+ *  enum: the first symbolic value in the enum value list
+ *
+ *  - Variable with built-in type: Variable(std::string name,DataType * type, ConstValue * initialValue);
+ *  - Compound Variable: Create all sub-variables with their initalValue, create a new variable and pass them as a list
+ *
+ */
+class Variable : public VariableTemplate<Variable> {
+ public:
+  Variable(const std::string &name,
+           const DataType *type,
+           ConstValue *initial_value = nullptr,
+           Variable *parent = nullptr,
+           LocationInfo location_info = LocationInfo());
 
-        //Visitor
-        virtual void accept(AbstractVisitor &visitor);
+  //Visitor
+  virtual void accept(AbstractVisitor &visitor);
 
-    };
+};
 
 }
-
 
 #endif //SCAM_MEMBER_H
