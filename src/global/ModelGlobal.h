58a71fb3cbd693c4cbdab15f0091937549436194
schwarz <michael.schwarz@lubis-eda.com>
2020-10-27 16:04:13 +0100

    CLang dir incorrectly set due to new install policy.

--- src/global/ModelGlobal.h
+++ src/global/ModelGlobal.h
@@ -2,8 +2,8 @@
 // Created by tobias on 30.12.16.
 //
 
-#ifndef SCAM_MODELGLOBAL_H
-#define SCAM_MODELGLOBAL_H
+#ifndef SCAM_MODEL_GLOBAL_H
+#define SCAM_MODEL_GLOBAL_H
 
 #include <stdexcept>
 #include "Config.h"
@@ -38,9 +38,10 @@ class ModelGlobal {
               IModelFactory *model_factory) {
 
     std::vector<std::string> result;
-    std::vector<const char *> commandLineArugmentsVector;
+    std::vector<const char *> command_line_arguments_vector;
     //Analyzing Environmental Variables          -----Default Values for Reference
-    std::string clang_dir = SCAM_HOME"/include/clang/3.4.2/include";//getenv("CLANG_DIR");
+//    std::string clang_dir = SCAM_HOME"/include/clang/3.4.2/include";//getenv("CLANG_DIR");
+    std::string clang_dir = SCAM_HOME"/bin";
     std::string systemc_dir = SCAM_HOME"/include/";//getenv("SYSTEMC_DIR");
     std::string scam_dir = SCAM_HOME"/";//getenv("SCAM_HOME");
     std::string root_dir = "/";//getenv("ROOT_DIR");
@@ -50,43 +51,43 @@ class ModelGlobal {
     if (root_dir.empty()) TERMINATE("Specify ROOT_DIR as environment variable");
 
     //Binary
-    commandLineArugmentsVector.push_back(Binary.c_str());
+    command_line_arguments_vector.push_back(Binary.c_str());
 
     //SRC-File to be analyzed
-    commandLineArugmentsVector.push_back(srcFile.c_str());
+    command_line_arguments_vector.push_back(srcFile.c_str());
 
     //Separator n
-    commandLineArugmentsVector.push_back("--");
+    command_line_arguments_vector.push_back("--");
 
     //TestCases include-dir: may not be changed
-    //commandLineArugmentsVector.push_back("-I /usr/include");
-    //commandLineArugmentsVector.push_back("-I /usr/local/include");
+    //command_line_arguments_vector.push_back("-I /usr/include");
+    //command_line_arguments_vector.push_back("-I /usr/local/include");
     std::string root_usr = std::string("-I" + root_dir + "usr/include/**");
-    commandLineArugmentsVector.push_back(root_usr.c_str());
+    command_line_arguments_vector.push_back(root_usr.c_str());
     std::string root_usr_local = std::string("-I" + root_dir + "usr/local/include/**");
-    commandLineArugmentsVector.push_back(root_usr_local.c_str());
+    command_line_arguments_vector.push_back(root_usr_local.c_str());
 
     //Include dirs
     std::string clang = std::string("-I" + clang_dir);
-    commandLineArugmentsVector.push_back(clang.c_str());
+    command_line_arguments_vector.push_back(clang.c_str());
     std::string systemc = std::string("-I" + systemc_dir);
-    commandLineArugmentsVector.push_back(systemc.c_str());
+    command_line_arguments_vector.push_back(systemc.c_str());
     std::string interfaces = std::string("-I" + scam_dir + "example/Interfaces/");
-    commandLineArugmentsVector.push_back(interfaces.c_str());
+    command_line_arguments_vector.push_back(interfaces.c_str());
     //Parameters for clang: may not be changed
     std::vector<const char *> clangParameter = {"-std=c++11", "-x", "c++", "-w", "-c"};
     for (auto item: clangParameter) {
-      commandLineArugmentsVector.push_back(item);
+      command_line_arguments_vector.push_back(item);
     }
     //Creates an instance of ModelFactory and calls ModelFactory::HandleTranslationUnit
-    const char *commandLineArgumentsArray[commandLineArugmentsVector.size()];
-    for (int i = 0; i < commandLineArugmentsVector.size(); i++) {
-      commandLineArgumentsArray[i] = commandLineArugmentsVector.at(i);
+    const char *commandLineArgumentsArray[command_line_arguments_vector.size()];
+    for (int i = 0; i < command_line_arguments_vector.size(); i++) {
+      commandLineArgumentsArray[i] = command_line_arguments_vector.at(i);
     }
 
     if (argc >= 1) {
       PluginAction
-          pa2(commandLineArugmentsVector.size(), commandLineArgumentsArray, model_factory);
+          pa2(command_line_arguments_vector.size(), commandLineArgumentsArray, model_factory);
     } else TERMINATE("Wrong use of DeSCAM");
   };
 
@@ -103,4 +104,4 @@ class ModelGlobal {
 };
 }
 
-#endif //SCAM_MODELGLOBAL_H
+#endif //SCAM_MODEL_GLOBAL_H
