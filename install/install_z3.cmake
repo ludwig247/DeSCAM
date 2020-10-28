c090efa7cbcd763c02a29755908065e4b973cd01
J3s7r <72141858+J3s7r@users.noreply.github.com>
2020-10-28 12:59:17 +0100

    Soft sprint 3 (#146)
    * CMake External libraries now install directly to the correct destination folder

--- install/install_z3.cmake
+++ install/install_z3.cmake
@@ -6,7 +6,7 @@ if (USE_SYSTEM_Z3)
 else ()
 
     include(ExternalProject)
-    ExternalProject_add(Z3-${Z3_VERSION}
+    ExternalProject_add(Z3
             # Location for external project with standard folder structure. Distinct by version
             PREFIX ${CMAKE_EXTERNAL_PROJECT_DIR}/z3/${Z3_VERSION}
             # Download the project from git via versioned tag. Checkout only the tag. Be verbose.
@@ -21,18 +21,19 @@ else ()
 
             # Install locally in the project
             CMAKE_ARGS
-            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+            -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_SOURCE_DIR}
+#            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
             -DCMAKE_BUILD_TYPE=Release
 
             INSTALL_COMMAND make install
             )
 
-    ExternalProject_Add_Step(Z3-${Z3_VERSION} SYMLINK
-            DEPENDEES install
-            COMMENT "Using libraries and headers of 'Z3' ${Z3_VERSION}"
-            # Create symbolic links for the chosen version. Change the link when switching versions.
-            COMMAND cp -a <INSTALL_DIR>/include/. ${CMAKE_CURRENT_SOURCE_DIR}/include/
-            COMMAND cp -a <INSTALL_DIR>/lib/. ${CMAKE_CURRENT_SOURCE_DIR}/lib/
+    ExternalProject_Add_Step(Z3 FORCED_INSTALL
+            DEPENDERS install
+            COMMAND ${CMAKE_COMMAND} -E echo "Installing Z3-${Z3_VERSION}"
+            COMMENT "Installing Z3-${Z3_VERSION}"
             ALWAYS TRUE
             )
+
+
 endif ()
