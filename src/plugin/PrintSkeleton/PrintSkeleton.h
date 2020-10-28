234a35b7ddc31e662582d1b42a3a53cf8b7db082
tulrich2 <tomulrich16@gmail.com>
2020-10-28 17:02:02 +0100

    - Removed the static definition of "int" as "signed(31 downto 0)" in the globalTypes.vhdl.
     - Integer arrays are now of type "signed(31 downto 0)" or "unsigned(31 downto 0)" instead of "int".
    
     - Fixed the bug that array signals are not correctly initialized in the reset part of a skeleton, if neither the complete array nor the element with index 0 are marked as variable in the module.

--- src/plugin/PrintSkeleton/PrintSkeleton.h
+++ src/plugin/PrintSkeleton/PrintSkeleton.h
@@ -7,6 +7,7 @@
 
 #include <PluginFactory.h>
 #include <sstream>
+#include <list>
 #include "Model.h"
 
 /*
@@ -86,6 +87,8 @@ private:
     void insertRegister(std::stringstream &ss, const std::string &name, const std::string &type);
 
     void insertNonblockingAssignment(std::stringstream &ss, const std::string &name, const std::string &value, int indentation);
+
+    bool arrayHasBeenReset(std::list<std::string> resetArrayList, std::string arrayName);
 
 
 };
