2b20884142f6a7ac8c39ab0059fe10e657ceb207
Tobias Ludwig <ludwig@eit.uni-kl.de>
2020-10-29 17:38:02 +0100

    Merge with development done.

--- tests/Optimizer_Test/src/CreateModel.h
+++ tests/Optimizer_Test/src/CreateModel.h
@@ -14,10 +14,45 @@
 #include <gtest/gtest.h>
 #include <Optimizer/Optimizer.h>
 
+#include "FindFunctions.h"
+#include "FindInitialValues.h"
+#include "FindModules.h"
+#include "FindNewDatatype.h"
+#include "FindPorts.h"
+#include "FindGlobal.h"
+#include "FindProcess.h"
+#include "FindVariables.h"
+#include "FindInstances.h"
+#include "FindStateName.h"
 
 static std::vector<DESCAM::Module *> createModules(std::string file_path) {
+  //Compositional root
+  std::unique_ptr<IFindModules> find_modules = std::make_unique<FindModules>();
+  std::unique_ptr<IFindNewDatatype> find_new_datatype = std::make_unique<FindNewDatatype>();
+  std::unique_ptr<IFindPorts> find_ports = std::make_unique<FindPorts>(find_new_datatype.get());
+  std::unique_ptr<IFindGlobal> find_global = std::make_unique<FindGlobal>();
+  std::unique_ptr<IFindInstances> find_instances = std::make_unique<FindInstances>();
+  std::unique_ptr<IFindStateName> find_state_name = std::make_unique<FindStateName>();
+  std::unique_ptr<IFindDataFlowFactory>
+      find_data_flow_factory = std::make_unique<FindDataFlowFactory>(find_state_name.get());
+  std::unique_ptr<IFindProcess> find_process = std::make_unique<FindProcess>(find_data_flow_factory.get());
+  std::unique_ptr<IFindInitialValues>
+      find_initial_values = std::make_unique<FindInitialValues>(find_data_flow_factory.get());
+  std::unique_ptr<IFindFunctions> find_functions =
+      std::make_unique<FindFunctions>(find_new_datatype.get(), find_data_flow_factory.get());
+  std::unique_ptr<IFindVariables> find_variables =
+      std::make_unique<FindVariables>(find_new_datatype.get(), find_initial_values.get(), find_data_flow_factory.get());
 
-    DESCAM::ModelGlobal::createModel(2, "DESCAM", file_path.c_str(),true);
+  auto model_factory = new ModelFactory(find_functions.get(),
+                                        find_modules.get(),
+                                        find_ports.get(),
+                                        find_global.get(),
+                                        find_process.get(),
+                                        find_variables.get(),
+                                        find_data_flow_factory.get(),
+                                        find_instances.get());
+
+  DESCAM::ModelGlobal::createModel(2, "DESCAM", file_path.c_str(),model_factory);
 
     std::vector<DESCAM::Module *> modulesVector;
     for (const auto &module: DESCAM::ModelGlobal::getModel()->getModules()) {
@@ -31,7 +66,35 @@ createModel(std::string file_path, const std::set<std::string> &optimizeOptionsS
     if (!optimizeOptionsSet.empty()) {
         CommandLineParameter::setOptimizeOptionsSet(optimizeOptionsSet);
     }
-    DESCAM::ModelGlobal::createModel(2, "DESCAM", file_path.c_str(), isWrapper);
+  //Compositional root
+  std::unique_ptr<IFindModules> find_modules = std::make_unique<FindModules>();
+  std::unique_ptr<IFindNewDatatype> find_new_datatype = std::make_unique<FindNewDatatype>();
+  std::unique_ptr<IFindPorts> find_ports = std::make_unique<FindPorts>(find_new_datatype.get());
+  std::unique_ptr<IFindGlobal> find_global = std::make_unique<FindGlobal>();
+  std::unique_ptr<IFindInstances> find_instances = std::make_unique<FindInstances>();
+  std::unique_ptr<IFindStateName> find_state_name = std::make_unique<FindStateName>();
+  std::unique_ptr<IFindDataFlowFactory>
+      find_data_flow_factory = std::make_unique<FindDataFlowFactory>(find_state_name.get());
+  std::unique_ptr<IFindProcess> find_process = std::make_unique<FindProcess>(find_data_flow_factory.get());
+  std::unique_ptr<IFindInitialValues>
+      find_initial_values = std::make_unique<FindInitialValues>(find_data_flow_factory.get());
+  std::unique_ptr<IFindFunctions> find_functions =
+      std::make_unique<FindFunctions>(find_new_datatype.get(), find_data_flow_factory.get());
+  std::unique_ptr<IFindVariables> find_variables =
+      std::make_unique<FindVariables>(find_new_datatype.get(), find_initial_values.get(), find_data_flow_factory.get());
+
+  auto model_factory = new ModelFactory(find_functions.get(),
+                                        find_modules.get(),
+                                        find_ports.get(),
+                                        find_global.get(),
+                                        find_process.get(),
+                                        find_variables.get(),
+                                        find_data_flow_factory.get(),
+                                        find_instances.get());
+
+
+
+    DESCAM::ModelGlobal::createModel(2, "DESCAM", file_path.c_str(), model_factory);
 
     std::vector<DESCAM::Model *> model;
     model.push_back(DESCAM::ModelGlobal::getModel());
