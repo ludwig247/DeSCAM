eadd42c80653b4d781713fced10814180ae227e7
schwarz <michael.schwarz@lubis-eda.com>
2020-11-10 18:21:43 +0100

    Added missing lib dependencies

--- install/install_llvm_git.cmake
+++ install/install_llvm_git.cmake
@@ -1,5 +1,5 @@
 
- #set(LLVM_ALL_PROJECTS "clang;clang-tools-extra;compiler-rt;debuginfo-tests;libc;libclc;libcxx;libcxxabi;libunwind;lld;lldb;mlir;openmp;parallel-libs;polly;pstl")
+
  include(ExternalProject)
  ExternalProject_add(LLVM
   # Location for external project with standard folder structure. Distinct by version
@@ -21,38 +21,33 @@
   -DLLVM_INCLUDE_TESTS=OFF
   -DLLVM_ENABLE_PROJECTS=clang;clang-tools-extra;openmp
   -DLLVM_BUILD_LLVM_DYLIB=ON
-  -DLLVM_USE_PERF=ON
-  -DLLVM_ENABLE_ZLIB=OFF
   # MAC OS may also need libcxx;libcxxabi"
 
   INSTALL_COMMAND make install
  )
 
- #    set(LLVM_INCLUDE_DIR ${CMAKE_EXTERNAL_PROJECT_DIR}/llvm/${LLVM_VERSION}/src/include)
- #    set(LLVM_BINARY_DIR ${CMAKE_EXTERNAL_PROJECT_DIR}/llvm/${LLVM_VERSION}/src/bin)
- #    set(LLVM_LIB_DIR ${CMAKE_EXTERNAL_PROJECT_DIR}/llvm/${LLVM_VERSION}/src/lib)
  set(LLVM_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/include)
  set(LLVM_BINARY_DIR ${CMAKE_SOURCE_DIR}/bin)
  set(LLVM_LIB_DIR ${CMAKE_SOURCE_DIR}/lib)
 
- ExternalProject_Add_Step(LLVM RTTI
-  DEPENDEES download
-  DEPENDERS build
-
-  COMMENT "Enabling RTTI for 'LLVM' ${LLVM_VERSION}"
-  # Insert RTTI requirement into CMakeLists.txt
-  # FIXME This inserts the RTTI repeatedly. Check if it's already in the file.
-  COMMAND echo set (LLVM_REQUIRES_RTTI 1) | cat - ${CMAKE_EXTERNAL_PROJECT_DIR}/llvm/${LLVM_VERSION}/src/LLVM/llvm/CMakeLists.txt > tmp
-  COMMAND mv tmp ${CMAKE_EXTERNAL_PROJECT_DIR}/llvm/${LLVM_VERSION}/src/LLVM/llvm/CMakeLists.txt
-  ALWAYS TRUE
- )
-
- ExternalProject_Add_Step(LLVM FORCED_INSTALL
-  DEPENDERS install
-  COMMAND ${CMAKE_COMMAND} -E echo "Installing LLVM-${LLVM_VERSION}"
-  COMMENT "Installing  LLVM-${LLVM_VERSION}"
-  ALWAYS TRUE
- )
+# ExternalProject_Add_Step(LLVM RTTI
+#  DEPENDEES download
+#  DEPENDERS build
+#
+#  COMMENT "Enabling RTTI for 'LLVM' ${LLVM_VERSION}"
+#  # Insert RTTI requirement into CMakeLists.txt
+#  # FIXME This inserts the RTTI repeatedly. Check if it's already in the file.
+#  COMMAND echo set (LLVM_REQUIRES_RTTI 1) | cat - ${CMAKE_EXTERNAL_PROJECT_DIR}/llvm/${LLVM_VERSION}/src/LLVM/llvm/CMakeLists.txt > tmp
+#  COMMAND mv tmp ${CMAKE_EXTERNAL_PROJECT_DIR}/llvm/${LLVM_VERSION}/src/LLVM/llvm/CMakeLists.txt
+#  ALWAYS TRUE
+# )
+#
+# ExternalProject_Add_Step(LLVM FORCED_INSTALL
+#  DEPENDERS install
+#  COMMAND ${CMAKE_COMMAND} -E echo "Installing LLVM-${LLVM_VERSION}"
+#  COMMENT "Installing  LLVM-${LLVM_VERSION}"
+#  ALWAYS TRUE
+# )
 
  if (LLVM_VERSION VERSION_LESS 4.0.0)
  ExternalProject_Add_Step(LLVM CLANG
@@ -63,4 +58,3 @@
   COMMAND cp -r <SOURCE_DIR>/clang <SOURCE_DIR>/llvm/tools
  )
  endif()
- #ExternalProject_Add_StepTargets(LLVM CLANG)
